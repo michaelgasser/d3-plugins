e369524eec30a9fff23f597b3f6903dc7d325f26
michaelgasser <micgas@gmail.com>
2015-01-27 12:15:47 +0100

    Nicer link start / end
    - Links defined with 2 additional points.  The links starts and ends
    with a short horizontal part, ensuring that arrows can be placed
    properly.
    
    - Minimal curvature to ensure nicer visuals for links going backwards
    (so far only if you move a target node more left than a source node
    (proper backlinks not implemented yet).

--- sankey/sankey.js
+++ sankey/sankey.js
@@ -56,16 +56,20 @@ d3.sankey = function() {
     function link(d) {
       var x0 = d.source.x + d.source.dx,
           x1 = d.target.x,
-          xi = d3.interpolateNumber(x0, x1),
-          x2 = xi(curvature),
-          x3 = xi(1 - curvature),
+		  x4 = x0 + d.source.dx,
+		  x5 = x1 - d.target.dx,
+          xi = d3.interpolateNumber(x0, x1), 
+          x2 = Math.max(xi(curvature), x4+d.dy),
+          x3 = Math.min(xi(curvature), x5-d.dy),
           y0 = d.source.y + d.sy + d.dy / 2,
           y1 = d.target.y + d.ty + d.dy / 2;
       return "M" + x0 + "," + y0
-           + "C" + x2 + "," + y0
-           + " " + x3 + "," + y1
-           + " " + x1 + "," + y1;
-    }
+           + "L" + x4 + "," + y0
+		   + "C" + x2 + "," + y0
+		   + " " + x3 + "," + y1
+		   + " " + x5 + "," + y1
+           + "L" + x1 + "," + y1;
+    };
 
     link.curvature = function(_) {
       if (!arguments.length) return curvature;
